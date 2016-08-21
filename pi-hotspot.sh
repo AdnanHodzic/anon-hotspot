167f17d37c6bddfac819075384fc659ad62d6e89
Adnan Hodzic <adnan@hodzic.org>
2016-08-21 12:52:49 +0200

    debugged "configuring interfaces"

--- pi-hotspot.sh
+++ pi-hotspot.sh
@@ -47,13 +47,35 @@ interface wlan0
 EOL
 
 # configuring interfaces | disable wpa_supplicant interference
-echo -e "\nDisabling wpa_supplicant interface, backup location: /etc/network/interfaces.org.bak"
+echo -e "\nGenerating new interface file, backup location: /etc/network/interfaces.org.bak"
 cp /etc/network/interfaces /etc/network/interfaces.org.bak
 
-# match, delete next line
-sed -i '/iface wlan0 inet manual/{n;N;N;N;N;d}' /etc/network/interfaces
-# match, append new line
-sed -i '/\iface wlan0 inet manual/a #    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf' /etc/network/interfaces
+cat > /etc/network/interfaces << EOL
+# interfaces(5) file used by ifup(8) and ifdown(8)
+
+# Please note that this file is written to be used with dhcpcd
+# For static IP, consult /etc/dhcpcd.conf and 'man dhcpcd.conf'
+
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+iface eth0 inet manua
+
+allow-hotplug wlan0
+iface wlan0 inet static
+    address 172.24.1.1
+    netmask 255.255.255.0
+    network 172.24.1.0
+    broadcast 172.24.1.255
+#    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+#allow-hotplug wlan1
+#iface wlan1 inet manual
+#    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+EOL
 
 echo -e "\nRestarting dhcpd\n"
 service dhcpcd restart
